a1c70f2b70cc5105317e3382f53a6f14c0b410d4
Elina Varenyk <elinavarenyk23@gmail.com>
2026-03-01 20:13:26 +0200

    Add files via upload

---
+++ 12 Integer To Roman/12 Integer To Roman.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<string> roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        vector<int> integer = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+        string res = "";
+
+        for (int i = 0; i < integer.size(); i++)
+        {
+            while (num >= integer[i])
+            {
+                num -= integer[i];
+                res += roman[i];
+            }
+        }
+
+        return res;
+    }
+};
+int main()
+{
+    Solution a;
+    cout << a.intToRoman(3749) << "\n";
+}
