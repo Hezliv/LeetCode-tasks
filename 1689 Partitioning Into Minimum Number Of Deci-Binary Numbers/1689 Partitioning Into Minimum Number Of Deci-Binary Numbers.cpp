47604218655c84d9157a087d7fba210a4780d8ae
Elina Varenyk <elinavarenyk23@gmail.com>
2026-03-01 20:17:01 +0200

    Add files via upload

---
+++ 1689 Partitioning Into Minimum Number Of Deci-Binary Numbers/1689 Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minPartitions(string n) {
+        int maxi = 0;
+        for (auto c : n)
+        {
+            if (maxi < c)
+                maxi = c;
+        }
+
+        return maxi - '0';
+    }
+};
+
+int main()
+{
+    Solution a;
+    cout << a.minPartitions("32");
+}
+
