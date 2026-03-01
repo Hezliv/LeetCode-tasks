091f9865590df7842708135bcfa9d2f8fb14bebe
Elina Varenyk <elinavarenyk23@gmail.com>
2026-03-01 20:14:48 +0200

    Add files via upload

---
+++ 1653 Minimum deletions to make string balanced/1653 Minimum deletions to make string balanced.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int del = 0;
+        int b_count = 0;
+        
+        int n = s.size();
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (s[i] == 'b')
+            {
+                b_count++;
+                
+            }
+            else
+            {
+                if (b_count > 0)
+                {
+                    del = min(del + 1, b_count);
+                }
+            }
+        }
+        return del;
+    }
+};
+
+int main()
+{
+    Solution a;
+    cout << a.minimumDeletions("aababbab");
+}
